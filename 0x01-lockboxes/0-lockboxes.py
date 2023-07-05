143d51d245d64734a99a43e88755e63a731e56b5
ZintleZ <sesethuz@gmail.com>
2023-07-05 01:20:20 -0700

    Lockbox

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+def canUnlockAll(boxes):
+    num_boxes = len(boxes)
+    unlocked_boxes = [False] * num_boxes
+    unlocked_boxes[0] = True
+
+    keys = boxes[0]
+    while keys:
+        key = keys.pop()
+        if key < num_boxes and not unlocked_boxes[key]:
+            unlocked_boxes[key] = True
+            keys.extend(boxes[key])
+
+    return all(unlocked_boxes)
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,1 @@
+Lock that box
